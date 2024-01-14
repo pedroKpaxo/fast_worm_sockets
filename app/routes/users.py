cf4bb5ef418f4a0fd4e7664bd355dfae452faeae
pedrokpaxo <pedrograxxa@gmail.com>
2024-01-14 13:03:50 -0300

    ⛏️  Added MongoDb contanier and basic client utils

---
+++ app/routes/users.py
@@ -0,0 +1,43 @@
+from fastapi import APIRouter, Request
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
+
+from lib.mongo.mongo_client import get_db_handle
+
+
+users_router = APIRouter()
+templates = Jinja2Templates(directory="templates")
+
+
+@users_router.get("/users")
+async def users(request: Request, response_class=HTMLResponse):
+    """Returns all Users in the database."""
+    client = get_db_handle()
+    db = client["users"]
+    collection = db["users"]
+    print(await collection.find_one())
+    pass
+
+
+@users_router.post("/users")
+async def create_user(request: Request, response_class=HTMLResponse):
+    """Creates a new User in the database."""
+    pass
+
+
+@users_router.get("/users/{user_id}")
+async def get_user(request: Request, response_class=HTMLResponse):
+    """Returns a User from the database."""
+    pass
+
+
+@users_router.put("/users/{user_id}")
+async def update_user(request: Request, response_class=HTMLResponse):
+    """Updates a User in the database."""
+    pass
+
+
+@users_router.delete("/users/{user_id}")
+async def delete_user(request: Request, response_class=HTMLResponse):
+    """Deletes a User from the database."""
+    pass
---
+++ lib/mongo/mongo_client.py
@@ -0,0 +1,16 @@
+import os
+from motor.motor_asyncio import AsyncIOMotorClient
+
+
+def get_db_handle():
+    """
+    This function returns a handle to the MongoDB database.
+    Make sure to set the environment variables `MONGO_INITDB_ROOT_USERNAME` and
+    `MONGO_INITDB_ROOT_PASSWORD` to the username and password of the MongoDB
+    database.
+    """
+    # NOTE make sure to set the environment variables
+    user = os.environ.get('MONGO_INITDB_ROOT_USERNAME', '')
+    password = os.environ.get('MONGO_INITDB_ROOT_PASSWORD', '')
+    client = AsyncIOMotorClient(f'mongodb://{user}:{password}@mongodb:27017')
+    return client
